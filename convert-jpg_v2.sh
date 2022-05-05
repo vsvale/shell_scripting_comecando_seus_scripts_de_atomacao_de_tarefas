1351688ffe1da586a46101fe1e04f833343ec216
Vinicius da Silva Vale <viniciusdvale@gmail.com>
2022-05-05 13:41:43 -0300

    Add files via upload

---
+++ convert-jpg_v2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export PATH=/usr/bin:$PATH
+
+converte_imagem(){
+    local caminho_imagem=$1
+    local imagem_sem_extensao=$(ls $caminho_imagem | awk -F. '{ print $1 }')
+    convert $imagem_sem_extensao.jpg $imagem_sem_extensao.png
+
+
+varrer_diretorio(){
+    cd $1
+    for arquivo in *
+    do
+        local caminho_arquivo=$(find ../shell_wk_new/imagens-novos-livros -name $arquivo)
+        if [ -d $caminho_arquivo ]
+        then
+            varrer_diretorio $caminho_arquivo
+        else
+            converte_imagem $caminho_arquivo
+        fi
+    done
+}
+
+varrer_diretorio ../shell_wk_new/imagens-novos-livros
